ef4d23d9c1d727239728090826bc4fbd1dd796ba
Isabella Amaral <87509290+lsaamaral@users.noreply.github.com>
2023-08-10 09:37:59 -0300

    adjust_timezone.cpp

--- adjust_timezone.cpp
+++ adjust_timezone.cpp
@@ -1,66 +1,31 @@
-#include "adjust_timezone.hpp"
+#include <ctime>
 
-namespace {
-    // Funcao auxiliar para verificar se um ano e bissexto
-    bool isLeapYear(int year) {
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    }
+bool isLeapYear(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
 std::time_t adjustTimeToGMTMinus3(int year, int month, int day, int hour, int minutes, int seconds) {
-    // Verifica se o ano e bissexto e ajusta a quantidade de dias de fevereiro
+    // Array to store the number of days in each month
     int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    // Adjust for leap years
     if (isLeapYear(year)) {
-        daysInMonth[1] = 29; // Fevereiro tem 29 dias em anos bissextos
-    } else {
-        daysInMonth[1] = 28; // Fevereiro tem 28 dias em anos nao bissextos
+        daysInMonth[1] = 29; // February has 29 days in leap years
     }
-
-    // Verifica se o dia fornecido e valido para o mes atual
-    if (day > daysInMonth[month - 1]) {
-        // Se nao for valido, ajusta para o ultimo dia valido do mes
-        day = daysInMonth[month - 1];
-    }
-
-    // Ajusta os anos e meses se o fuso horario causar mudanca de mes ou ano
-    if (hour < 3) {
-        if (month == 1) { // Janeiro (mes 1)
-            month = 12; // Dezembro (mes 12)
-            year--;
-        } else {
-            month--;
-        }
-        // Verifica se o dia ajustado e valido para o mes anterior
-        if (day > daysInMonth[month - 1]) {
-            // Se nao for valido, ajusta para o ultimo dia valido do mes
-            day = daysInMonth[month - 1];
-        }
-    }
-
-    // Constroi a estrutura tm com os valores fornecidos
-    std::tm localTime;
+    
+    // Adjust the provided time to GMT -3 timezone
+    struct tm localTime;
     localTime.tm_sec = seconds;
     localTime.tm_min = minutes;
-    localTime.tm_hour = hour;
+    localTime.tm_hour = hour - 3; // Adjust for GMT -3 timezone
     localTime.tm_mday = day;
-    localTime.tm_mon = month - 1; // tm_mon representa o mes de 0 a 11
-    localTime.tm_year = year - 1900; // tm_year representa o ano desde 1900
-    localTime.tm_isdst = -1; // Configura o flag para determinar horario de verao (usando valor -1)
+    localTime.tm_mon = month - 1; // tm_mon represents the month from 0 to 11
+    localTime.tm_year = year - 1900; // tm_year represents the year since 1900
+    localTime.tm_isdst = -1; // Set the flag to determine daylight saving time (using value -1)
 
-    // Converte a estrutura tm para segundos desde a epoch (1 de janeiro de 1970)
+    // Normalize the time (handles changes in days, months, and years)
     std::time_t adjustedTime = std::mktime(&localTime);
 
-    // Adiciona o ajuste do fuso horario -3 em segundos
-    adjustedTime -= 3 * 3600;
-
-    // Calcula a diferenca em segundos entre o tempo ajustado e o original
-    int timeDifference = static_cast<int>(adjustedTime - std::mktime(&localTime));
-
-    // Verifica se o ajuste do fuso horario faz com que o tempo resultante seja menor que o original
-    if (timeDifference < 0) {
-        // Se for o caso, ajusta o tempo em mais 24 horas (86400 segundos) para corrigir esse problema
-        adjustedTime += 24 * 3600;
-    }
-
+    // Return the adjusted time
     return adjustedTime;
 }
