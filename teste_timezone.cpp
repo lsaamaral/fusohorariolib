0fc7bb699e10877cd90363b496414265ec41e627
Isabella Amaral <87509290+lsaamaral@users.noreply.github.com>
2023-08-10 09:40:09 -0300

    Update teste_timezone.cpp

--- teste_timezone.cpp
+++ teste_timezone.cpp
@@ -1,27 +1,29 @@
-#include <stdio.h>
+#include <iostream>
+#include <ctime>
 #include "adjust_timezone.hpp"
 
-int main(){
-    int year = 0, month = 0, day = 0, hour = 0, minutes = 0, seconds = 0;
-    scanf("%d %d %d %d %d %d", &year, &month, &day, &hour, &minutes, &seconds);
+int main() {
+    std::string inputDateTime;
+    std::cout << "Enter date and time in format YYYYMMDD hh:mm:ss: ";
+    std::getline(std::cin, inputDateTime);
 
-    // Ajusta o tempo para o fuso horario -3
+    int year, month, day, hour, minutes, seconds;
+    if (sscanf(inputDateTime.c_str(), "%4d%2d%2d %2d:%2d:%2d", &year, &month, &day, &hour, &minutes, &seconds) != 6) {
+        std::cerr << "Invalid input format." << std::endl;
+        return 1;
+    }
+
+    // Adjust the time for GMT -3 timezone
     std::time_t adjustedTime = adjustTimeToGMTMinus3(year, month, day, hour, minutes, seconds);
 
-    // Converte o tempo ajustado para a estrutura tm
-    struct tm *timeinfo;
-    timeinfo = localtime(&adjustedTime);
+    // Convert the time_t value to a tm structure for display
+    struct tm* adjustedTm = std::localtime(&adjustedTime);
 
-    // Obtem os componentes da data e hora ajustados
-    year = timeinfo->tm_year + 1900;
-    month = timeinfo->tm_mon + 1;
-    day = timeinfo->tm_mday;
-    hour = timeinfo->tm_hour;
-    minutes = timeinfo->tm_min;
-    seconds = timeinfo->tm_sec;
+    // Format the adjusted date and time
+    char adjustedDateTime[20];
+    std::strftime(adjustedDateTime, sizeof(adjustedDateTime), "%Y%m%d %H:%M:%S", adjustedTm);
 
-    // Imprime os valores ajustados
-    printf("%d %d %d %d %d %d\n", year, month, day, hour, minutes, seconds);
+    std::cout << "Adjusted date and time: " << adjustedDateTime << std::endl;
 
     return 0;
 }
